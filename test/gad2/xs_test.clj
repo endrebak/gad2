b8eaf87e4518b1eda748b8265588e77a18670f7a
Endre Bakken Stovner <endrebak85@gmail.com>
2020-06-18 10:25:44 +0200

    Hiya

---
+++ test/gad2/xs_test.clj
@@ -0,0 +1,41 @@
+(ns gad2.xs-test
+  (:require [gad2.xs :refer :all]
+            [gad2.rulegraph :refer :all]
+            [gad2.examples.snakemake-example :refer [snakemake-rules xs]]
+            [midje.sweet :as midje])
+  (:use [midje.sweet]))
+
+(def dwx (deps-with-xs (rulegraph snakemake-rules) snakemake-rules))
+
+(facts "about `deps-with-xs`"
+       dwx
+
+       =>
+
+       '([:bcftools-call [:genome] :samtools-index [:sample :genome]]
+         [:bcftools-call [:genome] :samtools-sort [:sample :genome]]
+         [:plot-quals [:genome] :bcftools-call [:genome]]
+         [:samtools-index [:sample :genome] :samtools-sort [:sample :genome]]
+         [:samtools-sort [:sample :genome] :bwa-map [:sample :genome]])
+       )
+
+(facts "about `precompute-xs`"
+
+       (precompute-xs dwx xs)
+
+       =>
+
+       {[[:genome] [:genome]] {{:genome "hg19"} [{:genome "hg19"}]
+                               {:genome "hg38"} [{:genome "hg38"}]}
+        [[:genome] [:sample :genome]] {{:genome "hg19"} [{:genome "hg19" :sample "A"}
+                                                         {:genome "hg19" :sample "B"}]
+                                       {:genome "hg38"} [{:genome "hg38" :sample "A"}
+                                                         {:genome "hg38" :sample "B"}]}
+        [[:sample :genome] [:sample :genome]] {{:genome "hg19" :sample "A"} [{:genome "hg19"
+                                                                              :sample "A"}]
+                                               {:genome "hg19" :sample "B"} [{:genome "hg19"
+                                                                              :sample "B"}]
+                                               {:genome "hg38" :sample "A"} [{:genome "hg38"
+                                                                              :sample "A"}]
+                                               {:genome "hg38" :sample "B"} [{:genome "hg38"
+                                                                              :sample "B"}]}})
