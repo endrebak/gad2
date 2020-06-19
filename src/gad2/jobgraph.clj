73da1a14dfd8477ae16bd997eedb12394ff1bfc5
Endre Bakken Stovner <endrebak85@gmail.com>
2020-06-19 11:10:57 +0200

    Update

--- src/gad2/jobgraph.clj
+++ src/gad2/jobgraph.clj
@@ -1,5 +1,5 @@
 (ns gad2.jobgraph
-  (:require [gad2.xs :refer :all]))
+  (:require [gad2.xs :refer [deps-with-xs precompute-xs]]))
 
 
 (defn get-files
@@ -9,23 +9,31 @@
     fs))
 
 
-(defn combine-xs-&-targets
-  [xs outfiles child infiles parent]
+(defn combine-xs-&-ruletargets
+  [xs outfiles child]
   (apply concat
-         (for [[xcs xps] xs
-               infile infiles
+         (for [[xcs _] xs
                outfile outfiles]
+           {[outfile xcs] [child xcs]})))
+
+
+(defn combine-xs-&-targets
+  [xs outfiles child infiles]
+  (apply merge
+         (for [[xcs xps] xs
+               outfile outfiles
+               infile infiles]
            {[outfile xcs] [child xcs]
-            [child xcs] (vec (for [xp xps] [infile xp]))})))
+            [child xcs] [infile xps]})))
 
 
 (defn jobgraph
   [rulegraph rules xs]
   (let [dwx (deps-with-xs rulegraph rules)
         pxs (precompute-xs dwx xs)]
-    (for [[child childxs parent parentxs] dwx
-          :let [xmap (pxs [childxs parentxs])
-                outfiles (get-files (rules child))
-                infiles (get-files (rules parent))
-                nxs (pxs [childxs parentxs])]]
-      (combine-xs-&-targets nxs outfiles child infiles parent))))
+    (apply merge
+           (for [[child childxs parent parentxs] dwx
+                 :let [outfiles (get-files (rules child))
+                       infiles (get-files (rules parent))
+                       nxs (pxs [childxs parentxs])]]
+             (combine-xs-&-targets nxs outfiles child infiles)))))
